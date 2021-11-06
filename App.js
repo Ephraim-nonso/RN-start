d1bcb31e0b487e717cc2eb4cdd0c2aa2a58ce541
Chukwu Ephraim <Chukwuchinonsoephraim@gmail.com>
2021-11-06 20:20:50 +0100

    first-react-native-app

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 
 export default function App() {
+  const [state, setState] = useState("head");
+
+  const flipCoin = () => {
+    const random = Math.floor(Math.random() * 2) + 1;
+    console.log(random);
+    if (random === 2) {
+      setState("tail");
+    } else {
+      setState("head");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {state === "head" ? (
+        <Image
+          source={require("./assets/coin_h.png")}
+          style={{
+            width: 150,
+            height: 150,
+          }}
+        />
+      ) : (
+        <Image
+          source={require("./assets/coin_t.png")}
+          style={{
+            width: 150,
+            height: 150,
+          }}
+        />
+      )}
+
+      <TouchableOpacity style={styles.btnPrimary} onPress={flipCoin}>
+        <Text style={styles.textStyle}>Flip the Coin</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +44,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  btnPrimary: {
+    backgroundColor: "red",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 4,
+    marginTop: 30,
+  },
+
+  textStyle: {
+    color: "#fff",
   },
 });
